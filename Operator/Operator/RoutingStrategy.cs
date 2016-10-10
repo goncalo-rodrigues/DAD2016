c1e358665928c5401284db764fcf0b463e1347db
Tellma Correia <t.correia222@gmail.com>
2016-10-10 21:06:43 +0100

    RoutingStrategy Structure

---
+++ Operator/Operator/RoutingStrategy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator
+{
+    class RoutingStrategy
+    {
+        //TODO: change string for url type
+        public string chooseURL(List<string> urls, CTuple tuple) {
+            return "";
+        }
+    }
+}
