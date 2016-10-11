51887264b34fc099a64430a1686bbb4149bbb153
Goncalo <goncaloalfredorodrigues@gmail.com>
2016-10-11 11:58:44 +0100

    Replica structure. Operations (filter, dup, etc) structure.

---
+++ Operator/Operator/Operations.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator
+{
+    class Operations
+    {
+        public static ProcessDelegate GetDupOperation()
+        {
+            return new ProcessDelegate((x) => x);
+        }
+
+        public static ProcessDelegate GetUniqOperation(int fieldNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static ProcessDelegate GetCountOperation()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static ProcessDelegate GetFilterOperation(int fieldNumber, string comparator, string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static ProcessDelegate GetCustomOperation(string dll, string className, string method)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Operator/Operator/Replica.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator
+{
+    public delegate IList<string> ProcessDelegate(IList<string> tuple);
+    class Replica : MarshalByRefObject
+    {
+        public string OperatorId { get; }
+        private readonly ProcessDelegate processFunction;
+        private IList<Destination> destinations;
+        private IList<Replica> myReplicas;
+        //private Semantic semantic;
+
+
+        public Replica(string replicaInfo)
+        {
+        }
+        //public Replica(IDictionary<string,object> replicaInfo)
+        //{
+
+        //}
+
+        private CTuple Process(CTuple tuple)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SendToAll(CTuple tuple)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ProcessAndForward(CTuple tuple)
+        {
+            var result = Process(tuple);
+            SendToAll(result);
+        }
+    }
+}
