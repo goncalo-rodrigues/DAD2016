66d406371209aaf262f9357b00fd85cc3243219e
goncalo-rodrigues <metarl.kurtcoban@gmail.com>
2016-10-20 11:19:40 +0100

    pcs service is not a service anymore

---
+++ ProcessCreationService/ProcessCreationService/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessCreationService
+{
+    class Program
+    {
+        public static string processName;
+        public static PCServer server;
+        static void Main(string[] args)
+        {
+            if (args.Length >= 1)
+            {
+                processName = args[0];
+            }
+            server = new PCServer();
+            server.StartServer();
+            Console.WriteLine("ProcessCreationService has started. Press Enter to exit.");
+            Console.ReadLine();
+            server.StopServer();
+        }
+    }
+}
