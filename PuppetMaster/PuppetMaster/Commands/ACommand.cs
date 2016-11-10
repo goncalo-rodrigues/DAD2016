9bd0ddb9be4e163d63bdff19cf0ceaae7729d143
Tellma Correia <t.correia222@gmail.com>
2016-11-10 22:22:41 +0000

    fix bug notify

--- PuppetMaster/PuppetMaster/Commands/ACommand.cs
+++ PuppetMaster/PuppetMaster/Commands/ACommand.cs
@@ -32,7 +32,7 @@ namespace PuppetMaster
                     toNotify += arg + " ";
                 }
             }
-            master?.getLogger()?.Notify((new Record($"{name} {toNotify}", DateTime.Now)));
+            master?.getLogger()?.Notify((new Record($"{name}", $"{toNotify}", DateTime.Now)));
         }
 
         public abstract void Dispatch(string[] args);
